082d41007619777f3016c1f4843a42158146425d
Shreyas Kasliwal <kasliwalshreyas07@gmail.com>
2022-10-03 10:56:43 +0530

    updates

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+
+
+  const titleChangeHandler=(event)=>{
+    console.log(event);
+  };
+  
+  return (
+    <form>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' onChange ={titleChangeHandler}/>
+        </div>
+
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input type='number' min='0.01' step='0.01'/>
+        </div>
+
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input type='date' min='2019-01-01' step='2019-12-31'/>
+        </div>
+
+      </div>
+ <div className='new-expense__control'>
+  <button> Add Expense</button>
+
+</div>
+    </form>
+  );
+}; 
+
+export default ExpenseForm;
