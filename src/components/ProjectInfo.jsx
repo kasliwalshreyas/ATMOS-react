4ec80d28a6055dff2430a710091908e4f8fffa63
Shreyas Kasliwal <kasliwalshreyas07@gmail.com>
2022-10-15 18:56:07 +0530

    added delete section function

--- src/components/ProjectInfo.jsx
+++ src/components/ProjectInfo.jsx
@@ -1,61 +1,40 @@
 import Nav from 'react-bootstrap/Nav';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
-import DropdownMenu from 'react-bootstrap/esm/DropdownMenu';
-import { useState } from 'react';
 
+const ProjectInfo = ({ isProfileClicked, setIsProfileClicked }) => {
 
-
-
-const ProjectInfo = ({isProfileClicked,setIsProfileClicked}) => {
-
-    const handleProfileClickedInside= (event) => {
+    const handleProfileClickedInside = (event) => {
         event.stopPropagation();
         setIsProfileClicked(true);
     };
 
-    
-
-
     return (
         <div className="project-info">
-            <div className = "project-name-and-logo">
+            <div className="project-name-and-logo">
                 <div className="project-logo">
-                    <img className="project-logo-img" src="https://img.icons8.com/dusk/64/000000/microsoft-project.png"/>
+                    <img className="project-logo-img" src="https://img.icons8.com/dusk/64/000000/microsoft-project.png" />
                 </div>
                 <div className="project-name-plus-extra-info">
-                    <div className = "name-and-track-log">
+                    <div className="name-and-track-log">
                         <div className="project-name">
                             <h3 className="project-name-heading">Customer Stories - Q4</h3>
                             <div className="info-favorite-logo">
-                                <img className="info-logo-img" src="https://img.icons8.com/material-outlined/24/000000/info--v1.png"/>
-                                <img className="favorite-logo-img" src="https://img.icons8.com/ios-filled/50/000000/star--v1.png"/>
+                                <img className="info-logo-img" src="https://img.icons8.com/material-outlined/24/000000/info--v1.png" />
+                                <img className="favorite-logo-img" src="https://img.icons8.com/ios-filled/50/000000/star--v1.png" />
                             </div>
                         </div>
                         <div className="track-log">
                             <p>🟩 On Track</p>
                         </div>
-                    </div>                    
-                </div>
-                <div className = "user-profiles" >
-                    <img id='user-profile' className = "user-profile" onClick={handleProfileClickedInside}  src="https://img.icons8.com/color/48/000000/name--v1.png"/>
-                    <div id='dropdownMenu-user-profile' className={isProfileClicked ? 'show':'hide'}> 
-                        <a href='#/user-profile' className='user-profile-dropdown-option'>Profile</a>
-                        <a href='#/admin-portal'className='user-profile-dropdown-option'>Admin Portal</a>
                     </div>
-                    
-                    {/* <Dropdown className='user-profile-dropdown-btn dropdown' title={<img className = "user-profile" src="https://img.icons8.com/color/48/000000/name--v1.png"/>}>      
-                        <Dropdown.Item href='#/user-profile' className='user-profile-dropdown-option' >User Profile</Dropdown.Item>
-                        <Dropdown.Divider />
-                        <Dropdown.Item href='#/admin-portal'className='user-profile-dropdown-option' >Admin Portal</Dropdown.Item>
-                    </Dropdown> */}
+                </div>
+                <div className="user-profiles" >
+                    <img id='user-profile' className="user-profile" onClick={handleProfileClickedInside} src="https://img.icons8.com/color/48/000000/name--v1.png" />
+                    <div id='dropdownMenu-user-profile' className={isProfileClicked ? 'show' : 'hide'}>
+                        <a href='/user-profile' className='user-profile-dropdown-option'>Profile</a>
+                        <a href='/admin-portal' className='user-profile-dropdown-option'>Admin Portal</a>
+                    </div>
                 </div>
             </div>
-            {/* <div className="nav-bar">
-                <a to="/overview" className="nav-option option-1">Overview</a>
-                <a to="/board" className="nav-option option-2">Board</a>
-            </div> */}
             <Nav className="nav-bar" as="ul">
                 <Nav.Item as="li">
                     <Nav.Link href="/overview" className="nav-option option-1" >Overview</Nav.Link>
