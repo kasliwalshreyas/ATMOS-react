b95d1b349c4db4319f4dd80e60c6313da94083a1
DY-2001 <89105143+DY-2001@users.noreply.github.com>
2022-10-24 04:49:04 +0530

    add further pages to CreateProject

--- src/pages/Projects/CreateProject.jsx
+++ src/pages/Projects/CreateProject.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Rightdiv from "./Rightdiv";
+import Navbar from "../../UI/Navbar";
 
 import styles from "./CreateProject.module.css";
 
@@ -12,10 +13,29 @@ const CreateProject = () => {
   const [isPending, setIsPending] = useState(false);
   const [sectionIDList, setsectionIDList] = useState("");
   const history = useNavigate();
+  const [nextPage, setnextPage] = useState(1);
+  const [projectStatement, setProjectStatement] = useState("");
+  const [projectMission, setProjectMission] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [projectGuidlines, setProjectGuidlines] = useState("");
+
+  function backToHome() {
+    setnextPage(1);
+    history("/Projects");
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const project = { projectName, MaxMember, type, sectionIDList };
+    const project = {
+      projectName,
+      MaxMember,
+      type,
+      sectionIDList,
+      projectStatement,
+      projectMission,
+      projectDescription,
+      projectGuidlines,
+    };
     setIsPending(true);
     fetch("http://localhost:8000/projectList", {
       method: "POST",
@@ -23,46 +43,161 @@ const CreateProject = () => {
       body: JSON.stringify(project),
     }).then(() => {
       setIsPending(false);
+      setnextPage(1);
+      console.log(nextPage);
       history("/Projects");
     });
   };
 
+  const checkProjectName = () => {
+    // console.log(e.target.value);
+    let projectNameInput = document.getElementById("projectNameInput");
+    if (projectNameInput.value === "") {
+      projectNameInput.placeholder = "This field is required";
+    }
+  };
+
   return (
-    <div className={styles.createProject}>
-      <div className={styles.createProjectLeft}>
-        <div className={styles.create}>
-          <h1 className="new-project-head">New Project</h1>
-          <form onSubmit={handleSubmit}>
-            <label>Project name</label>
-            <input
-              type="text"
-              required
-              value={projectName}
-              onChange={(e) => setProjectName(e.target.value)}
-            />
-            <label>Max Members</label>
-            <input
-              type="text"
-              required
-              value={MaxMember}
-              onChange={(e) => setMaxMember(e.target.value)}
-            />
-            <label>Project type</label>
-            <select value={type} onChange={(e) => setType(e.target.value)}>
-              <option value="Personal">Personal</option>
-              <option value="Education">Education</option>
-              <option value="Business">Business</option>
-            </select>
-            {setIsPending && <button>Create</button>}
-            {!setIsPending && <button disabled>Adding Project...</button>}
-          </form>
+    <>
+      <Navbar />
+      <div className={styles.createProject}>
+        <div className={styles.projectLeftPart}>
+          <div className={styles.leftFix}>
+            <div className={styles.projectLeftUpperPart}>
+              <div className={styles.projectLeftUpperLeftPart}>
+                {nextPage === 1 && (
+                  <h1 className="new-project-head">New Project</h1>
+                )}
+                {nextPage !== 1 && (
+                  <h1 className="new-project-head">{projectName}</h1>
+                )}
+              </div>
+              <div className={styles.projectLeftUpperRightPart}>
+                <h1>{nextPage}/5</h1>
+              </div>
+            </div>
+
+            <div className={styles.projectLeftMiddlePart}>
+              {nextPage === 1 && (
+                <form>
+                  <label>Project name</label>
+                  <input
+                    id="projectNameInput"
+                    placeholder="Name"
+                    type="text"
+                    required
+                    value={projectName}
+                    onChange={(e) => setProjectName(e.target.value)}
+                  />
+                  {/* <label>Max Members</label>
+                <input
+                  type="text"
+                  required
+                  value={MaxMember}
+                  onChange={(e) => setMaxMember(e.target.value)}
+                /> */}
+                  <label>Project type</label>
+                  <select
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                  >
+                    <option value="Personal">Personal</option>
+                    <option value="Education">Education</option>
+                    <option value="Business">Business</option>
+                  </select>
+                </form>
+              )}
+
+              {nextPage === 2 && (
+                <form onSubmit={handleSubmit}>
+                  <label>Project Statement</label>
+                  <textarea
+                    className={styles.textareaInput}
+                    placeholder="Write about the purpose of the project."
+                    type="text"
+                    value={projectStatement}
+                    onChange={(e) => setProjectStatement(e.target.value)}
+                  />
+                </form>
+              )}
+
+              {nextPage === 3 && (
+                <form onSubmit={handleSubmit}>
+                  <label>Project Mission</label>
+                  <textarea
+                    placeholder="Write about the mission of the project."
+                    className={styles.textareaInput}
+                    type="text"
+                    value={projectMission}
+                    onChange={(e) => setProjectMission(e.target.value)}
+                  />
+                </form>
+              )}
+
+              {nextPage === 4 && (
+                <form>
+                  <label>Project Description</label>
+                  <textarea
+                    placeholder=" Give the Description about the project."
+                    className={styles.textareaInput}
+                    type="text"
+                    value={projectDescription}
+                    onChange={(e) => setProjectDescription(e.target.value)}
+                  />
+                </form>
+              )}
+
+              {nextPage === 5 && (
+                <form>
+                  <label>Project Guidlines</label>
+                  <textarea
+                    placeholder=" Write about the guidelines of the project."
+                    className={styles.textareaInput}
+                    type="text"
+                    value={projectGuidlines}
+                    onChange={(e) => setProjectGuidlines(e.target.value)}
+                  />
+                </form>
+              )}
+            </div>
+
+            <div className={styles.projectToggles}>
+              {nextPage === 1 && <button onClick={backToHome}>Back</button>}
+              {nextPage >= 2 && nextPage <= 5 && (
+                <button
+                  onClick={() => {
+                    setnextPage(nextPage - 1);
+                  }}
+                >
+                  Back
+                </button>
+              )}
+              {console.log(projectName)}
+              {nextPage >= 1 && nextPage <= 4 && (
+                <button
+                  onClick={() => {
+                    projectName === "" && checkProjectName();
+                    projectName !== "" && setnextPage(nextPage + 1);
+                  }}
+                >
+                  Next
+                </button>
+              )}
+              {nextPage === 5 && setIsPending && (
+                <button onClick={handleSubmit}>Setup Project</button>
+              )}
+              {nextPage === 5 && !setIsPending && (
+                <button disabled>Adding Project...</button>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="create-project-right">
+          <Rightdiv />
         </div>
       </div>
-
-      <div className="create-project-right">
-        <Rightdiv />
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Projects/Task/MainView.jsx
+++ src/pages/Projects/Task/MainView.jsx
@@ -4,33 +4,29 @@ import SectionArena from "./SectionArena";
 import { useState } from "react";
 import OverView from "../Overview/OverView";
 import Charts from "../Charts/Charts";
-import React from "react"
+import React from "react";
 
+const MainView = ({ overview, board, charts, from }) => {
+  //   console.log(from);
+  const [isProfileClicked, setIsProfileClicked] = useState(false);
 
-const MainView = ({ overview, board, charts }) => {
-    // console.log(props)
-    const [isProfileClicked, setIsProfileClicked] = useState(false);
+  const handleClickOutside = (event) => {
+    event.stopPropagation();
+    setIsProfileClicked(false);
+  };
 
-
-    const handleClickOutside = (event) => {
-        event.stopPropagation();
-        setIsProfileClicked(false);
-    };
-
-
-
-    return (
-        <div className="normal-div" onClick={handleClickOutside}>
-            <ProjectInfo
-                isProfileClicked={isProfileClicked}
-                setIsProfileClicked={setIsProfileClicked}
-            ></ProjectInfo>
-            <FilterFunc></FilterFunc>
-            {overview && <OverView  ></OverView>}
-            {board && <SectionArena></SectionArena>}
-            {charts && <Charts></Charts>}
-        </div>
-    );
-}
+  return (
+    <div className="normal-div" onClick={handleClickOutside}>
+      <ProjectInfo
+        isProfileClicked={isProfileClicked}
+        setIsProfileClicked={setIsProfileClicked}
+      ></ProjectInfo>
+      <FilterFunc></FilterFunc>
+      {overview && <OverView></OverView>}
+      {board && <SectionArena></SectionArena>}
+      {charts && <Charts></Charts>}
+    </div>
+  );
+};
 
 export default MainView;
