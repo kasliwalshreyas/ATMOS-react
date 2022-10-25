fdce83a1f2e69904e744e139651530be180492cb
Shreyas Kasliwal <kasliwalshreyas07@gmail.com>
2022-10-25 19:11:09 +0530

    added charts

---
+++ src/pages/Projects/Charts/CountChart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Charts.module.css';
+
+const CountChart = () => {
+    return (
+        <div className={styles.countChart}>
+
+        </div>
+    );
+}
+
+export default CountChart;
