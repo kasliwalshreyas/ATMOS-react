2dedf1014e387c0606df1da5902596ca475059d0
DY-2001 <89105143+DY-2001@users.noreply.github.com>
2022-10-27 22:47:58 +0530

    Set previous properties on few components

--- src/pages/Projects/ProjectList.jsx
+++ src/pages/Projects/ProjectList.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-
 const ProjectList = ({ projects }) => {
+  const navigate = useNavigate();
 
+  const insideProject = (project) => {
+    localStorage.setItem("projectId", project.id);
+    navigate("/task/overview");
+  };
+  projects.lastUsed = new Date();
   const handleLinkClick = (project) => {
-
-    
-    const navigate = useNavigate();
-  
-    const insideProject = (project) => {
-      localStorage.setItem('projectId', project.id);
-      navigate('/task/overview');
-    }
-    project.lastUsed = new Date();
-    
     fetch(`http://localhost:8000/projectList/${project.id}`, {
-            method: 'PUT',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type' : 'application/json'
-            },
-            body:JSON.stringify(project)
-        }).then((result) => {
-            return result.json()
-        })
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    }).then((result) => {
+      return result.json();
+    });
+  };
 
   return (
     <div className="projectlist">
@@ -37,14 +33,15 @@ const ProjectList = ({ projects }) => {
         .map((project) => (
           <div className="project-real">
             <div className="project-container">
-<<<<<<< HEAD
-              <Link onClick={() => {
+              {/* <Link onClick={() => {
                 handleLinkClick(project)
                 }}
-                to="/task" state={{ from: project.id }}>
-=======
-              <a onClick={() => { insideProject(project) }}>
->>>>>>> 15df0e06cce66f2a8de2aaf3185525f5148656a3
+                to="/task" state={{ from: project.id }}> */}
+              <a
+                onClick={() => {
+                  insideProject(project);
+                }}
+              >
                 <img
                   className="project-img"
                   src={`./images/img/img${project.id % 10}.PNG`}
