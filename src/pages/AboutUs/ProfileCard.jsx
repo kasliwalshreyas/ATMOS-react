42f7a5626a6fe91ccb7725d2d0e0b31ff4d150ff
Shreyas Kasliwal <kasliwalshreyas07@gmail.com>
2022-10-31 03:06:08 +0530

    contact us added

--- src/pages/AboutUs/ProfileCard.jsx
+++ src/pages/AboutUs/ProfileCard.jsx
@@ -4,19 +4,19 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 const ProfileCard = ({ name, email, image }) => {
     return (
         <>
-            <div class="box">
-                <div class="top-bar"></div>
-                <div class="top">
+            <div class="boxAboutUs">
+                <div class="top-barAboutUs"></div>
+                <div class="topAboutUs">
                     {/* <i class="fa fa-check-circle" aria-hidden="true"></i> */}
                     {/* <input type="checkbox" class="heart-btn" id="heart-btn-1" /> */}
                     {/* <label class="heart" for="heart-btn-1"></label> */}
                 </div>
-                <div class="content">
+                <div class="contentAboutUs">
                     <img src={image} alt="" />
                     <strong>{name}</strong>
                     <p>{email}</p>
                 </div>
-                <div className='btn'>
+                <div className='btnAboutUs'>
                     <a href="https://www.linkedin.com/in/dimitri-bertrand-2b1b4b1b9/" rel="noreferrer"><BsLinkedin /></a>
                     <a href="#" rel="noreferrer"><BsGithub /></a>
                 </div>
--- src/pages/ContactUs/Contact.jsx
+++ src/pages/ContactUs/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import OuterNavBar from "../../UI/OuterNavBar";
 import styles from './Contact.module.css';
 
 
@@ -19,81 +20,103 @@ const Contact = () => {
             + inputs.email + "&body="
             + messageEnter;
         document.location = request;
+
+        console.log(request, message);
     }
     return (
-        <div className={styles.contactMainView}>
-            <div className={styles.contactInfo}>
-                <div className={styles.contactInfoHead}>
-                    <h1>
-                        Get a Quote
-                    </h1>
-                    <p className={styles.para}> Fill Up the form and our team will get back to you within 24 hours</p>
+        <>
+            <OuterNavBar />
+            <div className={styles.contactUsMainView}>
+                <div className={styles.contactMainView}>
+                    <div className={styles.contactInfo}>
+                        <div className={styles.contactInfoHead}>
+                            <h1>
+                                Get a Quote
+                            </h1>
+                            <p className={styles.para2}> Fill Up the form and our team will get back to you within 24 hours</p>
 
-                </div>
-                <div className={styles.contactInfoBody}>
-                    <div className={styles.contactNumberDiv}>
-                    <img className={styles.phoneicon} src="https://img.icons8.com/color/48/000000/phone.png" />
-                    <p className={styles.para}>1234567890</p>
+                        </div>
+                        <div className={styles.contactInfoBody}>
+                            <div className={styles.contactNumberDiv}>
+                                <img className={styles.phoneicon} src="https://img.icons8.com/glyph-neue/64/000000/phone.png" />
+
+                                <div>
+                                    <p className={styles.para}>1234567890</p>
+                                </div>
+                            </div>
+                            <div className={styles.contactNumberDiv}>
+                                <img className={styles.mailicon} src="https://img.icons8.com/material-rounded/48/000000/marked-mail.png" />
+
+                                <div>
+                                    <p className={styles.para}>contactatmos@atmos.in</p>
+                                </div>
+                            </div>
+                            <div className={styles.contactNumberDiv}>
+                                <img className={styles.locationicon} src="https://img.icons8.com/glyph-neue/64/000000/user-location.png" />
+
+                                <div>
+                                    <p className={styles.para}>IIIT Sri City</p>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div className={styles.contactInfoFoot}>
+                            <img className={styles.contactSocialMediaImage} src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png" />
+                            <img className={styles.contactSocialMediaImage} src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v1.png" />
+                            <img className={styles.contactSocialMediaImage} src="https://img.icons8.com/sf-ultralight-filled/75/000000/twitter.png" />
+                        </div>
+
                     </div>
-                    <div className={styles.contactNumberDiv}>
-                        <img className={styles.mailicon} src="https://img.icons8.com/color/48/000000/filled-message.png" />
-                        <p className={styles.para}>contactatmos@atmos.in</p>
-                    </div>
-                    <div className={styles.contactNumberDiv}>
-                        <img className={styles.locationicon} src="https://img.icons8.com/doodle/48/000000/user-location.png" />
-                        <p className={styles.para}>IIIT Sri City</p>
-
-
+                    <div className={styles.contactForm}>
+                        <div className={styles.contactFormDiv}>
+                            <form
+                                onSubmit={e => sendMessage(e)}
+                                className="contact__form">
+                                <input
+                                    className={styles.field}
+                                    value={inputs.name}
+                                    onChange={e => handleInputChange(e)}
+                                    type="text" name="name"
+                                    placeholder="name"
+                                    title="Your name"
+                                    maxLength="50"
+                                    required />
+                                <input
+                                    className={styles.field}
+                                    value={inputs.email}
+                                    onChange={e => handleInputChange(e)}
+                                    type="email" name="email"
+                                    placeholder="email"
+                                    title="Your email"
+                                    maxLength="50"
+                                    required />
+                                <textarea
+                                    className={`${styles.textareaInput}, ${styles.field}`}
+                                    rows={7}
+                                    value={inputs.message}
+                                    onChange={e => handleInputChange(e)}
+                                    type="text" name="message"
+                                    placeholder="message"
+                                    title="Your message"
+                                    maxLength="550"
+                                    required />
+                                <input
+                                    className={styles.btn}
+                                    type="submit"
+                                    value="send message" />
+                            </form>
+                            {/* <h2>Contact Us</h2>
+                            <input type="text" className={styles.field} placeholder="Your Name" />
+                            <input type="text" className={styles.field} placeholder="Your Email" />
+                            <input type="text" className={styles.field} placeholder="Phone" />
+                            <textarea className={styles.textareaInput} placeholder="Message" ></textarea>
+                            <button className={styles.btn}>Send</button> */}
+                        </div>
                     </div>
                 </div>
-                <div className={styles.contactInfoFoot}>
-                    <img className={styles.contactSocialMediaImage} src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png" />
-                    <img className={styles.contactSocialMediaImage} src="https://img.icons8.com/fluency/48/000000/instagram-new.png" />
-
-
-                    <img className={styles.contactSocialMediaImage} src="https://img.icons8.com/fluency/48/000000/twitter.png" />
-
-
-
-                </div>
-
             </div>
-            <div className={styles.contactForm}>
-
-                <form
-                    onSubmit={e => sendMessage(e)}
-                    className="contact__form">
-                    <input
-                        value={inputs.name}
-                        onChange={e => handleInputChange(e)}
-                        type="text" name="name"
-                        placeholder="name"
-                        title="Your name"
-                        maxLength="50"
-                        required />
-                    <input
-                        value={inputs.email}
-                        onChange={e => handleInputChange(e)}
-                        type="email" name="email"
-                        placeholder="email"
-                        title="Your email"
-                        maxLength="50"
-                        required />
-                    <textarea
-                        value={inputs.message}
-                        onChange={e => handleInputChange(e)}
-                        type="text" name="message"
-                        placeholder="message"
-                        title="Your message"
-                        maxLength="550"
-                        required />
-                    <input
-                        type="submit"
-                        value="send message" />
-                </form>
-
-            </div>
-        </div>
+        </>
     );
 }
 
