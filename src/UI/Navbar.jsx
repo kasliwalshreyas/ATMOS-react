e474e7813316dad6313a9a0b018b2669ecbc7404
naman42624 <75940862+naman42624@users.noreply.github.com>
2022-12-06 22:15:38 +0530

    navbar

--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         </Link>
         <Link
           className="link"
-          style={{ padding: "0px 40px 0px 30px" }}
+          
           to="/messages"
         >
           Messages
