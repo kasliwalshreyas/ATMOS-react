84d8074e6b1451850188e27e180dbd81bc8eab68
Akash Yadav <81186992+AK-aShH@users.noreply.github.com>
2022-12-07 04:07:31 +0530

    Admin UI

--- src/pages/Admin-2/UserList/UserList.jsx
+++ src/pages/Admin-2/UserList/UserList.jsx
@@ -4,7 +4,24 @@ import {List, Datagrid, TextField, EmailField, EditButton, DeleteButton, ShowBut
 const UserList = (props) => {
     return (
         <List {...props}>
-            <Datagrid>
+            <Datagrid size="medium" sx={{
+                '& .RaDatagrid-row': {
+                    backgroundColor: '#54a6f7',
+                    color: '#032649',
+                },
+                '& .RaDatagrid-row:hover': {
+                    backgroundColor: '#032649',
+                    color: '#54a6f7',
+                },
+                '& .RaDatagrid-row:active': {
+                    backgroundColor: '#032649',
+                    color: '#54a6f7',
+                },
+                '& .RaDatagrid-thead > tr > th': {
+                    backgroundColor: '#032649',
+                    color: '#54a6f7',
+                }
+            }}>
                 <TextField source="id" />
                 <TextField source="userName" />
                 <EmailField source="emailId" />
